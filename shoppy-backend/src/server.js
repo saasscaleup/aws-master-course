96e115a692dccddb9fdefcb680649d26b89261c5
dani <dataextractai@gmail.com>
2024-07-17 14:46:59 -0700

    three-tier-architecture - added cpu load

--- shoppy-backend/src/server.js
+++ shoppy-backend/src/server.js
@@ -19,6 +19,17 @@ app.use('/api/auth', authRoutes);
 app.use('/api/items', itemRoutes);
 
 
+// Recursive function to calculate Fibonacci number
+function fibonacci(n) {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// Health Check Route
+app.get('/', (req, res) => {
+    res.status(200).send('Hello from Shoppy-Backend!');
+});
+
 // Health Check Route
 app.get('/health', (req, res) => {
     res.status(200).send({ status: 'OK', message: 'Server is healthy!' });
@@ -38,8 +49,22 @@ app.get('/database-health', (req, res) => {
         res.status(200).send({status: 'OK', message: 'Database is healthy!',result: results});
         }
     );
- 
 });
+
+// CPU-intensive algorithm 
+app.get('/cpu-load/:n', (req, res) => {
+
+    const n = parseInt(req.params.n, 10); // Parse the parameter as an integer
+
+    if (isNaN(n) || n < 0) {
+        return res.status(400).send('Invalid input. Please provide a non-negative integer.');
+    }
+
+    const result = fibonacci(n);
+    console.log(`Fibonacci number at position ${n} is ${result}`);
+    res.send(`Fibonacci number at position ${n} is ${result}`);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
